158311d89897efdaf1d1cc5a18736356c61bf17d
Talha Mansoor <talhaulhaque53@gmail.com>
2026-09-11 19:39:59 +0500

    chore: removed cors due to proxy approach on frontend dev, static in prod

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
 
@@ -20,7 +20,7 @@ const unknownEndpoint = (request, response, next) => {
 
 // ========================================
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 // app.use(requestLogger);
 app.use(express.static("dist"));
